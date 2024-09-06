464e6b742df6d09db3d885c813a4ff437325b74c
Nighly5143 <klientoy5143@gmail.com>
2024-09-06 08:04:19 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Clent Aspe';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  '4th year College in Bachelor of Science in Information technology.';
+export const ABOUT_TEXT_2 =
+  'Responsible for developing and maintaining web applications from the frontend and backend.';
+export const ABOUT_TEXT_3 =
+  'Versatile professional proficient in both frontend and backend development, capable of building complete web applications from start to finish. Possess a strong understanding of various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Software Developer with 3+ years of experience building websites and web applications using modern JavaScript tools/frameworks. Depth knowledge of front-end and back-end technologies and database  management.';
+export const RESUME_TEXT_2 =
+  'Software Developer with 3+ years of experience building websites and web applications using modern JavaScript  tools/frameworks.';
